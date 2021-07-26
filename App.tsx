a12749bd11e404f72505d606bf76cabfc887c855
GauravBuchchhas <gaurav@ripenapps.in>
2021-07-26 15:32:27 +0530

    connection established with aws

--- App.tsx
+++ App.tsx
@@ -1,19 +1,34 @@
 import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
 
+// import Auth from 'aws-amplify';
 import { withAuthenticator } from 'aws-amplify-react-native';
 
-import Amplify from 'aws-amplify'
+// import Amplify from 'aws-amplify'
+import Amplify from '@aws-amplify/core';
+import Auth from '@aws-amplify/auth';
 import config from './src/aws-exports';
 Amplify.configure(config)
 
+
+
 function App() {
+
+  // run this only when app is first mounted
+useEffect(() => {
+  const fetchUser = async () => {
+    const userInfo = await Auth.currentAuthenticatedUser({ bypassCache: true });
+    console.log('userInfo', userInfo);
+  }
+  fetchUser();
+}, [])
+
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
 

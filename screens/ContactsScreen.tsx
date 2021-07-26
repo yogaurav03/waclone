9f365e7de6528c47245d9344a95ffc575807273e
GauravBuchchhas <gaurav@ripenapps.in>
2021-07-26 17:36:25 +0530

    add users from api

--- screens/ContactsScreen.tsx
+++ screens/ContactsScreen.tsx
@@ -1,16 +1,39 @@
-import * as React from 'react';
+import React, {useEffect, useState} from 'react';
 import { StyleSheet, FlatList } from 'react-native';
 import { View } from '../components/Themed';
 import ContactListItem from '../components/ContactListItem';
-import Users from '../data/Users';
 import NewMessageButton from '../components/NewMessageButton';
+import API from '@aws-amplify/api';
+import {
+  graphqlOperation,
+} from 'aws-amplify';
+import { listUsers }  from '../src/graphql/queries';
 
 export default function ContactScreen() {
+
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const usersData = await API.graphql(
+          graphqlOperation(
+            listUsers
+          )
+        )
+        setUsers(usersData.data.listUsers.items);
+      } catch (e) {
+        console.log(e);
+      }
+    }
+    fetchUsers();
+  }, [])
+
   return (
     <View style={styles.container}>
       <FlatList
       style={{width: '100%'}}
-      data={Users}
+      data={users}
       renderItem={({ item }) => <ContactListItem user={item} />}
       keyExtractor={(item) => item.id} />
 
